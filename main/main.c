048a9ce3809b76b646b42d4aa8428cb951eea13b
matrzask <maciejtrzask@student.agh.edu.pl>
2024-10-29 15:49:10 +0100

    connect to wifi

--- main/main.c
+++ main/main.c
@@ -1,6 +1,63 @@
 #include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#define WIFI_SSID "OnePlus6"
+#define WIFI_PASS "12345678"
+#define BLINK_GPIO 2
+
+bool connected = false;
+
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_STA_START)
+    {
+        printf("WIFI CONNECTING....\n");
+    }
+    else if (event_id == WIFI_EVENT_STA_CONNECTED)
+    {
+        printf("WiFi CONNECTED\n");
+    }
+    else if (event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        printf("WiFi lost connection\n");
+        esp_wifi_connect();
+        printf("Retrying to Connect...\n");
+    }
+    else if (event_id == IP_EVENT_STA_GOT_IP)
+    {
+        printf("Wifi got IP...\n\n");
+    }
+}
+
+void wifi_connection()
+{
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&wifi_initiation);
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+    wifi_config_t wifi_configuration = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+        }};
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
+    esp_wifi_start();
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_connect();
+}
 
 void app_main(void)
 {
-
+    nvs_flash_init();
+    wifi_connection();
 }
