65004163156a99e062db4e5ea0976ffdda322baa
matrzask <maciejtrzask@student.agh.edu.pl>
2024-12-21 14:40:10 +0100

    removed unused defines

--- main/i2c.c
+++ main/i2c.c
@@ -6,8 +6,6 @@
 #define I2C_MASTER_SDA_IO 21
 #define I2C_MASTER_NUM I2C_NUM_0
 #define I2C_MASTER_FREQ_HZ 100000
-#define I2C_MASTER_TX_BUF_DISABLE 0
-#define I2C_MASTER_RX_BUF_DISABLE 0
 #define I2C_MASTER_TIMEOUT_MS 1000
 
 void i2c_master_init(i2c_master_bus_handle_t *bus_handle)
